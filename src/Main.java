f30a7323d1a544475118aca043aa2fbd668e2b95
delyanastoyanova <delyanastoyanova96@gmail.com>
2017-10-17 11:42:49 +0200

    Main done for handling user input.

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,82 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Created by Nikolay on 01-Oct-17.
- */
 
 public class Main {
+    int userChoice;
+    //Polynomial structure
     Polynomial polynomial;
+    //Field structure
     Field field;
-    int modulus,coefficient;
-    void computation(){
+    int exponent, prime;
+
+
+    void computation() {
+        System.out.println("Choose one of the operations: \n " +
+                "   Objects to be constructed:\n" +
+                "1. Integer modulus p, where p is prime.\n" +
+                "2. Polynomials with integer coefficients mod p.\n" +
+                "3. Finite Field with p^n elements.\n" +
+                "   Arithmetic with polynomials:\n" +
+                "4. Two polynomials with coefficients modulo prime, produce sum, scalar multiple,difference and product.\n" +
+                "5. Quotient and remainder of two polynomials(long division).\n" +
+                "6.Extented Euclidean algorithm for two polynomials with coefficients mod p.\n" +
+                "7. Decide whether two polynomials in mod p are equal to third polynomial mod p.\n" +
+                "   Arithmetic in finite fields:\n" +
+                "8. Addition and multiplication tables of irreducible polynomial.\n" +
+                "9.Produce sum, product and quotient of two irreducible polynomials.\n" +
+                "10. Test irreducibility of polynomial mod p and produce irreducible polynomials of prescribed degree.");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a polynomial");
-        coefficient = scanner.nextInt();
-        System.out.println("Enter the modulus");
-        modulus = scanner.nextInt();
-        field= new Field(coefficient,modulus);
-        field.generate();
-        field.print();
+        userChoice = scanner.nextInt();
+        switch (userChoice) {
+            case 1:
+                break;
+            case 2:
+                System.out.println("Enter modulus p.");
+                break;
+            case 3:
+                System.out.println("Enter a prime number p");
+                prime = scanner.nextInt();
+                if (!primeCheck(prime)) {
+                    System.out.println("The number is not prime.");
+                    System.exit(0);
+                }
+                System.out.println("Enter the exponent n.");
+                exponent = scanner.nextInt();
+                field = new Field(prime, exponent);
+                field.generate();
+                field.print();
+                System.out.println("\n The field consists of " + field.field.size() + " elements.");
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
 
+        }
 
+    }
+
+    boolean primeCheck(int p) {
+        if (p == 2) {
+            return true;
+        }
+        if (p % 2 == 0) return false;// the number is even
+        for (int i = 3; i * i <= p; i += 2) { //check of being divisible by odd number
+            if (p % i == 0)
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
