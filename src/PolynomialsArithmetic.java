1b284f1b3d679681ed56dcc93007b8b398d17d82
delyanastoyanova <delyanastoyanova96@gmail.com>
2017-10-17 22:13:59 +0200

    small change

--- src/PolynomialsArithmetic.java
+++ src/PolynomialsArithmetic.java
@@ -8,15 +8,18 @@ public class PolynomialsArithmetic {
     }
 
     Polynomial sum() {
-        Polynomial result = null;
-        int[] firstPoly= poly1.getCoefficient();
-        int[] secondPoly= poly2.getCoefficient();
-        int length=secondPoly.length;
-         if(firstPoly.length>secondPoly.length){
-             length=firstPoly.length;
-         }
-        int[] coeffResult=new int[length];
-        result.setCoefficient(coeffResult);
+//        int[] firstPoly= poly1.getCoefficient();
+//        int[] secondPoly= poly2.getCoefficient();
+//        int length=secondPoly.length;
+//        if(firstPoly.length>secondPoly.length){
+//             length=firstPoly.length;
+//         }
+//
+//        int[] coeffResult=new int[length];
+
+        Polynomial result = new Polynomial(0, Math.max(poly1.getDegree(), poly2.getDegree()));
+        for (int i = 0; i <= poly1.getDegree(); i++) result.coefficient[i] += poly1.coefficient[i];
+        for (int i = 0; i <= poly2.getDegree(); i++) result.coefficient[i] += poly2.coefficient[i];
         return result;
     }
 
