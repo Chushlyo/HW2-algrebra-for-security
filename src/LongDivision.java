02692825d8f1bc19614981ae98b3c41099e504ce
Chushlyo <nikolay.p.stoyanov@gmail.com>
2017-10-22 19:54:42 +0200

    EEC + LD

--- src/LongDivision.java
+++ src/LongDivision.java
@@ -14,6 +14,12 @@ public class LongDivision {
         NewPolynomial q = new NewPolynomial("0");
         NewPolynomial r= n1;
         
+        if (n1.getDegree()==0 && d1.getDegree()==0){
+            this.quot = new NewPolynomial(""+(n1.getThisCoef(0)/d1.getThisCoef(0)));
+            this.rem = new NewPolynomial(""+(n1.getThisCoef(0)%d1.getThisCoef(0)));
+            return;
+        }
+        
         if(n1.getDegree()<d1.getDegree()){
             this.rem = n1;
             this.quot = q;
@@ -27,7 +33,12 @@ public class LongDivision {
         
         if(n1.getDegree()==d1.getDegree()){
             for ( int i = 0; i<d1.getDegree();i++){
-                if (d1.getThisCoef(n1.getDegree()-1-i)>n1.getThisCoef(n1.getDegree()-1-i)){
+                d1.print();
+                n1.print();
+                if (d1.getThisCoef(d1.getDegree()-1-i)<n1.getThisCoef(n1.getDegree()-1-i)) break;
+                if (d1.getThisCoef(d1.getDegree()-1-i)>n1.getThisCoef(n1.getDegree()-1-i)){
+//                    System.out.println("1" + d1.getThisCoef(d1.getDegree()-1-i));
+//                    System.out.println("1" + n1.getThisCoef(n1.getDegree()-1-i));
                     this.rem = n1;
                     this.quot = q;
                     System.out.println("2The quotient is:");
@@ -41,7 +52,11 @@ public class LongDivision {
         while (r.getDegree()>=d1.getDegree() && r.getDegree()!=0){
 //            System.out.println("resu?");
             //divide leading terms
-            int lcr_lcb = r.getThisCoef(r.getDegree())/ d1.getThisCoef(d1.getDegree());
+            int lcr_lcb;
+            if ( r.getThisCoef(r.getDegree()) < d1.getThisCoef(d1.getDegree()))
+                lcr_lcb = d1.getThisCoef(d1.getDegree());
+            else lcr_lcb = r.getThisCoef(r.getDegree())/ d1.getThisCoef(d1.getDegree());
+            System.out.println("lcrss:" + lcr_lcb);
             NewPolynomial q2 = new NewPolynomial(lcr_lcb+"x^"+ (r.getDegree()-d1.getDegree()));
             q = pa.sum(q, q2).primeVersion(p);
             NewPolynomial r2 = new NewPolynomial(lcr_lcb+"x^"+ (r.getDegree()-d1.getDegree()));
